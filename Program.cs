0d4f20ca6ed7b2da1240a1392a397dc1cec721c4
cristiandz23 <cristiandanielzarate95@gmail.com>
2025-05-02 01:46:45 -0300

    Eliminacion de lineas comentadas

--- Program.cs
+++ Program.cs
@@ -47,6 +47,9 @@ namespace Dsw2025Ej8
 
                 }
             }
+
+
+            
         }
 
         private static void MostrarTituloCuenta(CuentaBancaria cuentaa)
@@ -54,7 +57,7 @@ namespace Dsw2025Ej8
             Console.Clear();
             Console.SetCursorPosition(20, Console.CursorTop);
             Console.WriteLine($"Titular/es de la cuenta: {string.Join(", ", cuentaa.Titulares).ToUpper()} - " +
-                                $"Numero de cuenta: {cuentaa.Numero} - "+
+                                $"Numero de cuenta: {cuentaa.Numero} - " +
                                 $"Tipo de cuenta: {cuentaa.GetType().Name} ");
             Console.WriteLine();
         }
@@ -63,7 +66,7 @@ namespace Dsw2025Ej8
         {
             MostrarTituloCuenta(cuenta1);
 
-            Console.WriteLine($"--- {mensaje} ---");
+            Console.WriteLine($"--- {mensaje} ---\n\n");
             Console.WriteLine(cuenta1);
 
             Console.WriteLine("\nPresione una tecla para continuar...");
@@ -75,125 +78,15 @@ namespace Dsw2025Ej8
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"\n[ERROR] {ex.Message}");
+                Console.WriteLine($"\n[ERROR] {ex.Message}\n");
             }
 
             Console.WriteLine(cuenta1);
-            Console.WriteLine("\nPresione una tecla para continuar...");
+            Console.WriteLine("\nPresione una tecla... \n");
             Console.ReadKey();
             Console.Clear();
         }
 
-        //public static void Diisplay(CuentaBancaria[] cuenta)
-        //{
-        //    foreach (var cuentaBancaria in cuenta)
-        //    {
-        //        MostrarTituloCuenta(cuentaBancaria);
-
-        //        RealizarOperacion("Realizar depósito de 1000", () => cuentaBancaria.Depositar(1000M), cuentaBancaria);
-
-        //        RealizarOperacion("Retirar todo el saldo", () => cuentaBancaria.Retirar(cuentaBancaria.Saldo), cuentaBancaria);
-
-        //        RealizarOperacion("Retirar dinero sin tener saldo", () => cuentaBancaria.Retirar(12M), cuentaBancaria);
-
-        //        RealizarOperacion("Realizar depósito con cuenta suspendida", () => cuentaBancaria.Depositar(1000M), cuentaBancaria);
-        //    }
-        //}
-
-        //private static void MostrarTituloCuenta(CuentaBancaria cuentaa)
-        //{
-        //    Console.Clear();
-        //    Console.SetCursorPosition(20, Console.CursorTop);
-        //    Console.WriteLine($"TITULAR DE CUENTA: {string.Join(", ", cuentaa.Titulares)}".ToUpper());
-        //    Console.WriteLine($"\n{cuentaa}\n");
-        //}
-
-        //private static void RealizarOperacion(string mensaje, Action accion, CuentaBancaria cuenta1)
-        //{
-        //    Console.WriteLine(mensaje);
-        //    Console.ReadKey();
-
-        //    try
-        //    {
-        //        accion();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine($"[ERROR] {ex.Message}");
-        //    }
-
-        //    Console.WriteLine($"\n{cuenta1}\n");
-        //    Console.WriteLine("Presione una tecla para continuar...");
-        //    Console.ReadKey();
-        //    Console.Clear();
-        //}
-
-
-        //public static void Display(CuentaBancaria[] cuentas)
-        //{
-        //    foreach (CuentaBancaria cuenta in cuentas)
-        //    {
-        //        Console.SetCursorPosition(20, Console.CursorTop); // mueve el cursor 20 columnas a la derecha
-        //        Console.WriteLine($"TITULAR DE CUENTA: {string.Join(", ", cuenta.Titulares)}".ToUpper());
-
-        //        Console.WriteLine($"\n {cuenta}\n");
-        //        Console.WriteLine("Realizar deposito de 1000");
-        //        Console.ReadKey();
-        //        cuenta.Depositar(1000M);
-        //        Console.WriteLine($"\n {cuenta}\n");
-        //        Console.ReadKey();
-
-        //        Console.Clear();
-
-        //        Console.SetCursorPosition(20, Console.CursorTop); // mueve el cursor 20 columnas a la derecha
-        //        Console.WriteLine($"TITULAR DE CUENTA: {string.Join(", ", cuenta.Titulares)}".ToUpper());
-        //        Console.WriteLine($"\n {cuenta}\n");
-        //        Console.WriteLine("Retirar todo el saldo");
-        //        Console.ReadKey();
-        //        try
-        //        {
-        //            cuenta.Retirar(cuenta.Saldo);
-
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine(ex.Message);
-        //        }
-        //        Console.WriteLine($"\n {cuenta}\n");
-        //        Console.ReadKey();
-
-        //        Console.Clear();
-
-        //        Console.SetCursorPosition(20, Console.CursorTop); // mueve el cursor 20 columnas a la derecha
-        //        Console.WriteLine($"TITULAR DE CUENTA: {string.Join(", ", cuenta.Titulares)}".ToUpper());
-        //        Console.WriteLine($"\n {cuenta}\n");
-        //        Console.WriteLine("Retirar dinero sin tener saldo\n");
-        //        Console.ReadKey();
-        //        try
-        //        {
-        //            cuenta.Retirar(12M);
-
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine(ex.Message);
-        //        }
-        //        Console.WriteLine($"\n {cuenta}\n");
-        //        Console.ReadKey();
-        //        Console.Clear();
-        //        Console.WriteLine($"\n {cuenta}\n");
-        //        Console.WriteLine("Realizar deposito con cuenta suspendida\n");
-        //        try
-        //        {
-        //            cuenta.Depositar(1000);
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine(ex.Message);
-        //        }
-        //        Console.ReadKey();
-        //        Console.Clear();
-        //    }
-        //}
+       
     }
 }
